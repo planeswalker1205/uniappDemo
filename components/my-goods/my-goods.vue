a05c792bf7263970d6de6964d96c5831ed064b98
planeswalker1205 <462083214@qq.com>
2022-07-06 19:47:36 +0800

    完成了商品列表功能

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,66 @@
+<template>
+	<view>
+			<view class="good-item">
+				<view class="good-logo">
+					<image :src="item.goods_small_logo||defaulPic" mode=""></image>
+				</view>
+				<view class="good-info">
+					<text class="good-describe">{{item.goods_name||''}}</text>
+					<text class="good-price">￥{{item.goods_price | tofixed}}</text>
+				</view>
+			</view>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-goods",
+		data() {
+			return {
+				defaulPic:'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+			}
+		},
+		filters:{
+			tofixed(num){
+				return Number(num).toFixed(2)
+			}
+		},
+		props:{
+			item:{
+				type:Object,
+				default:{}
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+.good-item{
+	display: flex;
+	margin:10px 0;
+	border-bottom: 1px solid #efefef;
+	padding: 10px 5px;
+	.good-logo{
+		image{
+			width: 100px;
+			height: 100px;
+		}
+	}
+	.good-info{
+		margin-left:5px;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		.good-describe{
+			font-size: 14px;
+		}
+		.good-price{
+			color:#ff0000;
+			font-size: 15px;
+			font-weight: bold;
+		}
+	}
+}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,11 @@
 <template>
 	<view>
-		goodslist
+		<view class="goodslist">
+			<view  v-for="(item,i) in goodsList" :key="i"  @click="gotoDetail(item)">
+					<my-goods :item="item"></my-goods>
+			</view>
+		
+		</view>
 	</view>
 </template>
 
@@ -8,8 +13,70 @@
 	export default {
 		data() {
 			return {
-				
+				//请求参数对象
+				queryObj:{
+					//查询关键词
+					query:'',
+					//商品分类Id
+					cid:'',
+					pagenum:1,
+					pagesize:10,
+					//商品列表的数据
+	
+				},
+				goodsList:[],
+				//总数量
+				total:0,
+				isRequest:false
 			};
+		},
+		onReachBottom(){
+			if(this.queryObj.pagenum*this.queryObj.pagesize>=this.total){
+				
+				return uni.$showMsg('已经到底啦！')
+			}
+			this.queryObj.pagenum = this.queryObj.pagenum+1 
+			this.getGoodsList()
+		},
+		onPullDownRefresh() {
+			this.queryObj.pagenum = 1
+			this.total = 0
+			this.isRequest = false
+			this.goodsList = []
+			//重发请求
+			this.getGoodsList(()=>uni.stopPullDownRefresh())
+		},
+		methods:{
+			async getGoodsList(cb){
+				if(this.isRequest) return
+				this.isRequest = true
+				try{
+					const {data:res} = await uni.$http.get('/api/public/v1/goods/search',this.queryObj)
+					if(res.meta.status!==200) return uni.$showMsg()
+					this.goodsList = [...this.goodsList,...res.message.goods]
+					this.total = res.message.total
+					this.isRequest = false
+					cb&&cb()
+				}catch(e){
+					//查询失败，数据回滚
+					this.queryObj.pagenum = this.queryObj.pagenum==1?1:this.queryObj.pagenum-1
+					this.isRequest = false
+					cb&&cb()
+				}
+				
+				
+			},
+			gotoDetail(item){
+				uni.navigateTo({
+					url:'/subpkg/goods_detail/goods_detail?goods_id='+item.goods_id
+				})
+			}
+		},
+
+		onLoad(options) {
+			this.queryObj.query = options.query||''
+			this.queryObj.cid = options.cid||''
+			this.getGoodsList()
 		}
 	}
 </script>
